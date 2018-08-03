03112df910c7d837e0ab3de9ac413515b4ef1246
fioretto@umich.edu <fioretto@umich.edu>
2018-08-03 08:31:33 -0400

    update to import function

--- implementations/dcgan/dcgan.py
+++ implementations/dcgan/dcgan.py
@@ -3,6 +3,11 @@ import os
 import numpy as np
 import math
 
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+
 import torchvision.transforms as transforms
 from torchvision.utils import save_image
 
